dcb2f1a6782ad1cf29bfd63b8fd5f47df1e20ab6
Giang <nguyenvietgiang1110@gmail.com>
2023-06-28 17:11:07 +0700

    Dựng UI Khung cho các component

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  validateForm!: FormGroup;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  submitForm(): void {
+    for (const i in this.validateForm.controls) {
+      this.validateForm.controls[i].markAsDirty();
+      this.validateForm.controls[i].updateValueAndValidity();
+    }
   }
 
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.validateForm = this.fb.group({
+      userName: [null, [Validators.required]],
+      password: [null, [Validators.required]],
+      remember: [true]
+    });
+  }
 }
--- src/app/pages/player-list/player-list.component.ts
+++ src/app/pages/player-list/player-list.component.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Person {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+}
+
 @Component({
   selector: 'app-player-list',
   templateUrl: './player-list.component.html',
   styleUrls: ['./player-list.component.css']
 })
 export class PlayerListComponent implements OnInit {
-
+  isVisible = false;
+  listOfData: Person[] = [
+    {
+      key: '1',
+      name: 'John Brown',
+      age: 32,
+      address: 'New York No. 1 Lake Park'
+    },
+    {
+      key: '2',
+      name: 'Jim Green',
+      age: 42,
+      address: 'London No. 1 Lake Park'
+    },
+    {
+      key: '3',
+      name: 'Joe Black',
+      age: 32,
+      address: 'Sidney No. 1 Lake Park'
+    }
+  ];
   constructor() { }
 
   ngOnInit(): void {
   }
+  
+  showModal(): void {
+    this.isVisible = true;
+  }
 
+  handleOk(): void {
+    console.log('Button ok clicked!');
+    this.isVisible = false;
+  }
+
+  handleCancel(): void {
+    console.log('Button cancel clicked!');
+    this.isVisible = false;
+  }
 }
