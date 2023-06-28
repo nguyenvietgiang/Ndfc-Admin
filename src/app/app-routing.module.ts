201735b25a7a33b683ae0f798d45feda1e8dd245
Giang <nguyenvietgiang1110@gmail.com>
2023-06-28 12:35:58 +0700

    Cấu hình Routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { PlayerListComponent } from './pages/player-list/player-list.component';
+import { DashbroadComponent } from './pages/dashbroad/dashbroad.component';
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
-  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
+{path:'', pathMatch:'full', redirectTo:'/login'},
+{path:'login',component:LoginComponent },
+{path:'welcome',component:WelcomeComponent},
+{path:'player',component:PlayerListComponent},
+{path:'dashbroad', component:DashbroadComponent}
+//  { path: '', pathMatch: 'full', redirectTo: '/welcome' },
+//  { path: 'welcome', loadChildren: () => import('./pages/welcome/welcome.module').then(m => m.WelcomeModule) }
 ];
 
 @NgModule({
